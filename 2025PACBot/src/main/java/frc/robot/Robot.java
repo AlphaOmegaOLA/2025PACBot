a5b37d5f98df68540600f811d09923d5ef9f6b6a
AlphaOmegaOLA <61032907+AlphaOmegaOLA@users.noreply.github.com>
2025-01-18 11:18:01 -0600

    Syncing recent changes

--- 2025PACBot/src/main/java/frc/robot/Robot.java
+++ 2025PACBot/src/main/java/frc/robot/Robot.java
@@ -4,105 +4,65 @@
 
 package frc.robot;
 
+import edu.wpi.first.util.sendable.SendableRegistry; 
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import edu.wpi.first.cameraserver.CameraServer;
+// USB Cameras
+import edu.wpi.first.cscore.UsbCamera;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
-public class Robot extends TimedRobot
-{
-  private Command m_autonomousCommand;
+/** This is a demo program showing how to use Mecanum control with the MecanumDrive class. */
+public class Robot extends TimedRobot {
+  // Top Camera
+  UsbCamera topcam;
+  private static final int kFrontLeftChannel = 4;
+  private static final int kRearLeftChannel = 3;
+  private static final int kFrontRightChannel = 1;
+  private static final int kRearRightChannel = 2;
 
-  private RobotContainer m_robotContainer;
+  private static final int kJoystickChannel = 0;
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+  private MecanumDrive m_robotDrive;
+  private XboxController m_stick;
+
   @Override
-  public void robotInit() 
-  {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
+  public void robotInit() {
+    CANSparkMax frontLeft = new CANSparkMax(kFrontLeftChannel, MotorType.kBrushless);
+    CANSparkMax rearLeft = new CANSparkMax(kRearLeftChannel, MotorType.kBrushless);
+    CANSparkMax frontRight = new CANSparkMax(kFrontRightChannel, MotorType.kBrushless);
+    CANSparkMax rearRight = new CANSparkMax(kRearRightChannel, MotorType.kBrushless);
+
+    SendableRegistry.addChild(m_robotDrive, frontLeft);
+    SendableRegistry.addChild(m_robotDrive, rearLeft);
+    SendableRegistry.addChild(m_robotDrive, frontRight);
+    SendableRegistry.addChild(m_robotDrive, rearRight);
+
+    // Invert the right side motors.
+    // You may need to change or remove this to match your robot.
+    frontRight.setInverted(true);
+    rearRight.setInverted(true);
+    frontLeft.setInverted(false);
+
+    topcam = CameraServer.startAutomaticCapture(0);
+
+
+    m_robotDrive = new MecanumDrive(frontLeft::set, rearLeft::set, frontRight::set, rearRight::set);
+
+    m_stick = new XboxController(kJoystickChannel);
   }
 
-  /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
   @Override
-  public void robotPeriodic() 
-  {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
-    CommandScheduler.getInstance().run();
+  public void teleopPeriodic() {
+    // Use the joystick Y axis for forward movement, X axis for lateral
+    // movement, and Z axis for rotation.
+    m_robotDrive.driveCartesian(-m_stick.getLeftY(), m_stick.getLeftX(), m_stick.getRightX());
+    // Slow Version for Testing
+    //m_robotDrive.driveCartesian(-m_stick.getLeftY()*.2, m_stick.getLeftX()*.2, m_stick.getRightX()*.2);
+    //m_robotDrive.driveCartesian(-m_stick.getLeftY()*.5, m_stick.getLeftX()*.5, m_stick.getRightX()*.5);
+    //m_robotDrive.driveCartesian(-m_stick.getLeftY()*.7, m_stick.getLeftX()*.7, m_stick.getRightX()*.7);
+    //m_robotDrive.driveCartesian(-m_stick.getLeftY()*.8, m_stick.getLeftX()*.8, m_stick.getRightX()*.8);
   }
-
-  /** This function is called once each time the robot enters Disabled mode. */
-  @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {}
-
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
-  @Override
-  public void autonomousInit() 
-  {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    /*
-     * String autoSelected = SmartDashboard.getString("Auto Selector",
-     * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-     * = new MyAutoCommand(); break; case "Default Auto": default:
-     * autonomousCommand = new ExampleCommand(); break; }
-     */
-
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
-  }
-
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {}
-
-  @Override
-  public void teleopInit() 
-  {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
-  }
-
-  /** This function is called periodically during operator control. */
-  @Override
-  public void teleopPeriodic() {}
-
-  @Override
-  public void testInit() 
-  {
-    // Cancels all running commands at the start of test mode.
-    CommandScheduler.getInstance().cancelAll();
-  }
-
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
 }
